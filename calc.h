5203599be8903c9c1cb08529a433ab144fc0a849
student <gks.econom@gmail.com>
2018-06-03 20:24:29 +0300

    c progect main structure

---
+++ calc.h
@@ -0,0 +1,15 @@
+#ifndef POLSK_CALC_CALC_H
+#define POLSK_CALC_CALC_H
+#define NUMBER '0'
+
+void push(double);
+
+double pop(void);
+
+int getop(char []);
+
+int getch(void);
+
+void ungetch(int);
+
+#endif //POLSK_CALC_CALC_H
